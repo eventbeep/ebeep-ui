0b4a2760b7a85886ad52f1057a77d72bd1fbdace
Venkatesh Prasad <venkateshprasadvm@gmail.com>
2020-10-09 22:52:21 +0530

    Updated icons

--- lib/shared/icons.dart
+++ lib/shared/icons.dart
@@ -12,7 +12,21 @@ class EBIcons {
 
   static const String _fontFamily = 'EBIcons';
 
-  static const IconData group = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData attachment = IconData(0xe910, fontFamily: _fontFamily);
+  static const IconData camera = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData claps_fill = IconData(0xe924, fontFamily: _fontFamily);
+  static const IconData documents = IconData(0xe925, fontFamily: _fontFamily);
+  static const IconData download = IconData(0xe929, fontFamily: _fontFamily);
+  static const IconData follow_user = IconData(0xe92a, fontFamily: _fontFamily);
+  static const IconData followed_user =
+      IconData(0xe92b, fontFamily: _fontFamily);
+  static const IconData gallery = IconData(0xe92c, fontFamily: _fontFamily);
+  static const IconData send = IconData(0xe92d, fontFamily: _fontFamily);
+  static const IconData tags = IconData(0xe92e, fontFamily: _fontFamily);
+  static const IconData done_circle = IconData(0xe92f, fontFamily: _fontFamily);
+  static const IconData video_gallery =
+      IconData(0xe930, fontFamily: _fontFamily);
+  static const IconData warning = IconData(0xe931, fontFamily: _fontFamily);
   static const IconData comment = IconData(0xe909, fontFamily: _fontFamily);
   static const IconData add_user = IconData(0xe900, fontFamily: _fontFamily);
   static const IconData back = IconData(0xe901, fontFamily: _fontFamily);
@@ -50,7 +64,6 @@ class EBIcons {
   static const IconData reward = IconData(0xe921, fontFamily: _fontFamily);
   static const IconData search = IconData(0xe922, fontFamily: _fontFamily);
   static const IconData see_more = IconData(0xe923, fontFamily: _fontFamily);
-  static const IconData send_fill = IconData(0xe924, fontFamily: _fontFamily);
   static const IconData share = IconData(0xe926, fontFamily: _fontFamily);
   static const IconData two_users = IconData(0xe927, fontFamily: _fontFamily);
   static const IconData user = IconData(0xe928, fontFamily: _fontFamily);
--- lib/widgets/text/read_more_text.dart
+++ lib/widgets/text/read_more_text.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_linkify/flutter_linkify.dart';
 
 import '../../colors/colors.dart';
 import '../../widgets.dart';
