30ba35f00806a40ec06cbcf6b5e5ece5bc3a7e02
KevalPrajapati <keval.prajapatimus@gmail.com>
2022-01-24 18:06:17 +0530

    Adds maxlines and maxHieght to EBTextField

--- lib/widgets/text_field/text_field.dart
+++ lib/widgets/text_field/text_field.dart
@@ -17,6 +17,8 @@ class EBTextField extends StatelessWidget {
     this.errorText,
     this.maxLength,
     this.maxLines,
+    this.minLines,
+    this.focusNode,
     this.controller,
     this.onTap,
     this.prefix,
@@ -25,6 +27,7 @@ class EBTextField extends StatelessWidget {
     this.enabled = true,
     this.readOnly = false,
     this.onSubmit,
+    this.maxHieght,
   }) : super(key: key);
 
   final TextEditingController? controller;
@@ -37,6 +40,8 @@ class EBTextField extends StatelessWidget {
   final String? hintText;
   final int? maxLength;
   final int? maxLines;
+  final int? minLines;
+  final FocusNode? focusNode;
   final Function? onChanged;
   final Function? onSubmit;
   final Function? onTap;
@@ -44,6 +49,7 @@ class EBTextField extends StatelessWidget {
   final Widget? prefix;
   final TextCapitalization textCapitalization;
   final TextInputType textInputType;
+  final double? maxHieght;
 
   @override
   Widget build(BuildContext context) {
@@ -55,39 +61,45 @@ class EBTextField extends StatelessWidget {
           Text(labelText!, style: EBTextStyles.label),
           EBSpacers.height8,
         ],
-        TextField(
-          readOnly: readOnly,
-          onSubmitted: onSubmit as void Function(String)?,
-          autofocus: autofocus,
-          onTap: onTap as void Function()?,
-          enabled: enabled,
-          controller: controller,
-          onChanged: onChanged as void Function(String)?,
-          textCapitalization: textCapitalization,
-          keyboardType: textInputType,
-          obscureText: isPassword,
-          maxLength: maxLength,
-          maxLines: maxLines,
-          buildCounter: (context,
-                  {required currentLength, maxLength, required isFocused}) =>
-              null,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderSide: BorderSide(width: 1, color: EBColors.primary),
-              borderRadius: BorderRadius.circular(EBDimens.borderRadius),
+        Container(
+          constraints: BoxConstraints(maxHeight: maxHieght ?? double.infinity),
+          child: TextField(
+            readOnly: readOnly,
+            onSubmitted: onSubmit as void Function(String)?,
+            autofocus: autofocus,
+            onTap: onTap as void Function()?,
+            enabled: enabled,
+            focusNode: focusNode,
+            controller: controller,
+            onChanged: onChanged as void Function(String)?,
+            textCapitalization: textCapitalization,
+            keyboardType: textInputType,
+            obscureText: isPassword,
+            maxLength: maxLength,
+            maxLines: maxLines,
+            minLines: minLines,
+            buildCounter: (context,
+                    {required currentLength, maxLength, required isFocused}) =>
+                null,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: EBColors.primary),
+                borderRadius: BorderRadius.circular(EBDimens.borderRadius),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2, color: EBColors.primary),
+                borderRadius: BorderRadius.circular(EBDimens.borderRadius),
+              ),
+              hintStyle:
+                  EBTextStyles.bodyText1.copyWith(color: EBColors.grey50),
+              hintText: hintText,
+              counterText: '',
+              errorText: errorText,
+              suffixIcon: suffix,
+              prefixIcon: prefix,
             ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 2, color: EBColors.primary),
-              borderRadius: BorderRadius.circular(EBDimens.borderRadius),
-            ),
-            hintStyle: EBTextStyles.bodyText1.copyWith(color: EBColors.grey50),
-            hintText: hintText,
-            counterText: '',
-            errorText: errorText,
-            suffixIcon: suffix,
-            prefixIcon: prefix,
           ),
-        ),
+        )
       ],
     );
   }
