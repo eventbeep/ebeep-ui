34b9fb72d2a202d0b7699e6e4371ef4821f47c1a
Ayush <ayushrajguru412@gmail.com>
2022-06-03 19:33:08 +0300

    style: new icons

--- lib/shared/beep_icons.dart
+++ lib/shared/beep_icons.dart
@@ -11,7 +11,7 @@
 ///      fonts:
 ///       - asset: fonts/BeepIcons.ttf
 ///
-///
+/// 
 /// * Linearicons Free, Copyright (C) Linearicons.com
 ///         Author:    Perxis
 ///         License:   CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0/)
@@ -25,50 +25,51 @@ class BeepIcons {
   static const _kFontFam = 'BeepIcons';
   static const String? _kFontPkg = null;
 
-  static const IconData arroba = IconData(0xe800, fontFamily: _kFontFam);
-  static const IconData attachment = IconData(0xe801, fontFamily: _kFontFam);
-  static const IconData bell_filled = IconData(0xe802, fontFamily: _kFontFam);
-  static const IconData bell = IconData(0xe803, fontFamily: _kFontFam);
-  static const IconData bin = IconData(0xe804, fontFamily: _kFontFam);
-  static const IconData checkmark_circle =
-      IconData(0xe805, fontFamily: _kFontFam);
-  static const IconData checkmark = IconData(0xe806, fontFamily: _kFontFam);
-  static const IconData close = IconData(0xe807, fontFamily: _kFontFam);
-  static const IconData degree = IconData(0xe808, fontFamily: _kFontFam);
-  static const IconData home_filled = IconData(0xe809, fontFamily: _kFontFam);
-  static const IconData home = IconData(0xe80a, fontFamily: _kFontFam);
-  static const IconData hyperlink = IconData(0xe80b, fontFamily: _kFontFam);
-  static const IconData logout = IconData(0xe80c, fontFamily: _kFontFam);
-  static const IconData mail = IconData(0xe80d, fontFamily: _kFontFam);
-  static const IconData file = IconData(0xe80e, fontFamily: _kFontFam);
-  static const IconData mic_filled = IconData(0xe80f, fontFamily: _kFontFam);
-  static const IconData mic = IconData(0xe810, fontFamily: _kFontFam);
-  static const IconData paper_plane = IconData(0xe811, fontFamily: _kFontFam);
-  static const IconData reply = IconData(0xe812, fontFamily: _kFontFam);
-  static const IconData reward_filled = IconData(0xe813, fontFamily: _kFontFam);
-  static const IconData reward = IconData(0xe814, fontFamily: _kFontFam);
-  static const IconData search = IconData(0xe815, fontFamily: _kFontFam);
-  static const IconData speech_bubble = IconData(0xe816, fontFamily: _kFontFam);
-  static const IconData speech_bubbles =
-      IconData(0xe817, fontFamily: _kFontFam);
-  static const IconData user_single = IconData(0xe818, fontFamily: _kFontFam);
-  static const IconData users = IconData(0xe819, fontFamily: _kFontFam);
-  static const IconData visible_off = IconData(0xe81a, fontFamily: _kFontFam);
-  static const IconData visible = IconData(0xe81b, fontFamily: _kFontFam);
-  static const IconData menu_dots = IconData(0xe81c, fontFamily: _kFontFam);
-  static const IconData mic_off_filled =
-      IconData(0xe81d, fontFamily: _kFontFam);
-  static const IconData clock = IconData(0xe81e, fontFamily: _kFontFam);
-  static const IconData calendar = IconData(0xe81f, fontFamily: _kFontFam);
-  static const IconData smiley = IconData(0xe820, fontFamily: _kFontFam);
-  static const IconData star_filled = IconData(0xe826, fontFamily: _kFontFam);
-  static const IconData star = IconData(0xe827, fontFamily: _kFontFam);
-  static const IconData share_filled = IconData(0xe828, fontFamily: _kFontFam);
-  static const IconData back_arrow = IconData(0xe829, fontFamily: _kFontFam);
-  static const IconData unpin = IconData(0xe82a, fontFamily: _kFontFam);
-  static const IconData camera = IconData(0xe82b, fontFamily: _kFontFam);
-  static const IconData clipboard = IconData(0xe82c, fontFamily: _kFontFam);
-  static const IconData images = IconData(0xe82d, fontFamily: _kFontFam);
-  static const IconData video = IconData(0xe82e, fontFamily: _kFontFam);
-  static const IconData pushpin = IconData(0xe832, fontFamily: _kFontFam);
+  static const IconData arroba = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData attachment = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bell_filled = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bell = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bin = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData checkmark_circle = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData checkmark = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData close = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData degree = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home_filled = IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home = IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData hyperlink = IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData logout = IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mail = IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData file = IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mic_filled = IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mic = IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData paper_plane = IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData reply = IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData reward_filled = IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData reward = IconData(0xe814, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search = IconData(0xe815, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData speech_bubble = IconData(0xe816, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData speech_bubbles = IconData(0xe817, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user_single = IconData(0xe818, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData users = IconData(0xe819, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData visible_off = IconData(0xe81a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData visible = IconData(0xe81b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData menu_dots = IconData(0xe81c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mic_off_filled = IconData(0xe81d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData clock = IconData(0xe81e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData calendar = IconData(0xe81f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData smiley = IconData(0xe820, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData hourglass = IconData(0xe821, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData question = IconData(0xe822, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData trophy = IconData(0xe823, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData star_filled = IconData(0xe826, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData star = IconData(0xe827, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData share_filled = IconData(0xe828, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData back_arrow = IconData(0xe829, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData unpin = IconData(0xe82a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData camera = IconData(0xe82b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData clipboard = IconData(0xe82c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData images = IconData(0xe82d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData video = IconData(0xe82e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData information = IconData(0xe82f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pushpin = IconData(0xe832, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
